c66571b62995b4ddeefaf0a1c4ee72697a3b7428
Sergey Kazenyuk <kazenyuk@gmail.com>
2013-04-07 04:04:45 +0400

    GC work-in-progress
    FASTA reader to be written

---
+++ gc-computing_gc_content.hs
@@ -0,0 +1,54 @@
+-- http://rosalind.info/problems/gc/
+import Data.List (intersperse)
+type DnaString = String
+data FastaString = FastaString String DnaString
+
+instance Show FastaString where
+  show (FastaString id dna) = ">" ++ id ++ "\n" ++ dna
+--  showList (x:[]) = (\s -> (show x) ++ s)
+--  showList (x:xs) = (\s -> (show x) ++ "\n" ++ show xs ++ s)
+  showList xs = (\s -> concat (intersperse "\n" $ map show xs) ++ s)
+  
+readFasta s = [ FastaString
+                "Rosalind_6404"
+                "CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACTAATAATTCTGAGG"
+              , FastaString
+                "Rosalind_5959"
+                "CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCTATATCCATTTGTCAGCAGACACGC" 
+              , FastaString
+                "Rosalind_0808"
+                "CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT"
+              ]
+           
+fastaId :: FastaString -> String
+fastaId (FastaString id _) = id
+
+fastaGcContent :: FastaString -> Float
+fastaGcContent (FastaString _ dna) = gcContent dna
+
+gcContent :: DnaString -> Float
+gcContent dna = (\(gc, len) -> gc / len * 100)
+              $ foldl count (0, 0) dna
+                where 
+                  count (gc, len) 'G' = (gc + 1, len + 1)
+                  count (gc, len) 'C' = (gc + 1, len + 1)
+                  count (gc, len)  _  = (gc,     len + 1)
+
+highestGcContent :: [FastaString] -> (String, Float)
+highestGcContent (x:xs) = foldl compareGcContent                                 
+                          (fastaId x, fastaGcContent x)
+                          xs
+                          where
+                            compareGcContent (id, content) e = if newContent > content
+                              then (fastaId e, newContent)
+                              else (id, content)
+                              where newContent = fastaGcContent e
+
+main :: IO ()
+main = do
+  rawFasta <- getContents
+  let fasta = readFasta rawFasta
+  
+  let (highestId, content) = highestGcContent fasta
+  putStrLn $ highestId ++ "\n" ++ (show content)
+

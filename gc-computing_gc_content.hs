cf628c7781cdc3e03009376f4a7e81ba469910d9
Sergey Kazenyuk <kazenyuk@gmail.com>
2013-05-17 06:24:15 +0400

    FASTA parser with test data
    FASTA entries with newlines in the DNA string are not parsed correctly now.

--- gc-computing_gc_content.hs
+++ gc-computing_gc_content.hs
@@ -1,5 +1,8 @@
 -- http://rosalind.info/problems/gc/
 import Data.List (intersperse)
+--import Control.Monad (liftM)
+import Text.ParserCombinators.Parsec
+
 type DnaString = String
 data FastaString = FastaString String DnaString
 
@@ -8,20 +11,57 @@ instance Show FastaString where
 --  showList (x:[]) = (\s -> (show x) ++ s)
 --  showList (x:xs) = (\s -> (show x) ++ "\n" ++ show xs ++ s)
   showList xs = (\s -> concat (intersperse "\n" $ map show xs) ++ s)
-  
-readFasta s = [ FastaString
-                "Rosalind_6404"
-                "CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCT\
-                \CCCACTAATAATTCTGAGG"
-              , FastaString
-                "Rosalind_5959"
-                "CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCTA\
-                \TATCCATTTGTCAGCAGACACGC"
-              , FastaString
-                "Rosalind_0808"
-                "CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACT\
-                \GGGAACCTGCGGGCAGTAGGTGGAAT"
-              ]
+
+parseFasta :: String -> [FastaString]
+parseFasta s = case (parse (parseFastaEntry `sepEndBy` newline) "fasta_source" s) of
+                 Left err -> error $ "Can't read FASTA: " ++ show err
+                 Right xs -> xs
+
+parseFastaEntry = do
+  name <- parseName
+  newline
+  dna <- parseDna
+  return (FastaString name dna)
+
+parseName = do
+  char '>'
+  name <- many1 $ alphaNum <|> char '_'
+  return name
+
+parseDna = do
+--  dna <- liftM concat $ (many1 $ oneOf "ACGTacgt") `sepBy` newline
+  dna <- many1 (oneOf "ACGTacgt")
+  return dna
+
+-- Test data
+n3 = ">Rosalind_6404\nCCTGCGGAAGATCGGCACTAGAATAGCCA\nGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACTAATAATTCTGAGG\n>Rosalind_5959\nCCATCGGTAGCGCATCCTTAG\nTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCTATATCCATTTGTCAGCAGACACGC\n>Rosalind_0808\nCCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCC\nGGACTGGGAACCTGCGGGCAGTAGGTGGAAT"
+n = ">Rosalind_6404\nCCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACTAATAATTCTGAGG\n>Rosalind_5959\nCCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCTATATCCATTTGTCAGCAGACACGC\n>Rosalind_0808\nCCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCC\nGGACTGGGAACCTGCGGGCAGTAGGTGGAAT"
+n2 = ">Rosalind_6404\nCCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACTAATAATTCTGAGG\n>Rosalind_5959\nCCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCTATATCCATTTGTCAGCAGACACGC\n>Rosalind_0808\nCCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT\n"
+
+nn2 = parseFasta n2
+nn3 = parseFasta n3
+nn = parseFasta n
+t2 = parseFasta . show
+tt = t2 t
+t = readFastaStub ""
+
+readFastaStub s = [ FastaString
+                    "Rosalind_6404"
+                    "CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCT\
+                    \CCCACTAATAATTCTGAGG"
+                  , FastaString
+                    "Rosalind_5959"
+                    "CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCTA\
+                    \TATCCATTTGTCAGCAGACACGC"
+                  , FastaString
+                    "Rosalind_0808"
+                    "CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACT\
+                    \GGGAACCTGCGGGCAGTAGGTGGAAT"
+                  ]
+-- End of test data
+
+readFasta = parseFasta
+--readFasta = readFastaStub
            
 fastaId :: FastaString -> String
 fastaId (FastaString id _) = id
